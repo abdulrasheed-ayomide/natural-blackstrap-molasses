e4c1bc81bee2f569019bb6b29d6743c058bf584d
ayomide <ayomiderasheed226@gmail.com>
2026-08-09 00:57:37 +0100

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/natural-blackstrap-molasses/',
   plugins: [react()],
   build: {
     sourcemap: false,
