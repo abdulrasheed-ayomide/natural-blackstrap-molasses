8b6b11311dbf244580a99b9eb16c62f64b513b6c
ayomide <ayomiderasheed226@gmail.com>
2026-08-09 01:10:50 +0100

    fixed wrong homepage path

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './styles/index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/natural-blackstrap-molasses">
       <CartProvider>
         <App />
       </CartProvider>
